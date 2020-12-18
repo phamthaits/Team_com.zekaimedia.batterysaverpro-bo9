c628a5ae78f9fe708575500b6e9988ac76d61ff5
Phong-PC <Tiotee.com@gmail.com>
2020-12-18 16:19:17 +0700

    Phong: Completed

--- app/src/main/java/app/tahachi/batterydoctor/fragment/fmBatterySaveMain.java
+++ app/src/main/java/app/tahachi/batterydoctor/fragment/fmBatterySaveMain.java
@@ -416,9 +416,9 @@ public class fmBatterySaveMain extends Fragment implements View.OnClickListener 
         intView(view);
         intData(view);
         intEvent();
-//        adControlHelp.loadNative(getActivity(), view.findViewById(R.id.native_ads_control_holder)
-//                , R.layout.item_admob_native_ad, R.layout.item_fb_native_ad,
-//                R.layout.item_mopub_native_ad,true,false);
+        adControlHelp.loadNative(getActivity(), view.findViewById(R.id.native_ads_control_holder)
+                , R.layout.item_admob_native_ad, R.layout.item_fb_native_ad,
+                R.layout.item_mopub_native_ad,true,false);
         adControlHelp.loadNative(getActivity(), view.findViewById(R.id.banner_native_ads_control_holder)
                 , R.layout.item_admob_banner_native, R.layout.item_fb_banner_native,
                 R.layout.item_mopub_banner_native,
